576d078d1582c523e6a1f1315788253daea0af1f
KKamil897 <soyari37>
2014-01-01 22:02:06 +0100

    Revert "KKamil897"
    This reverts commit e5d34d5ca0260c1be03a3c04d1303df1e1655860.

--- core/Mysql.php
+++
@@ -1,6 +0,0 @@
-<?php
-    define('mysql_host', 'localhost');
-	define('mysql_name', 'database');
-	define('mysql_user', 'mysql');
-    define('mysql_pass', '111');	
-?>
--- index.php
+++
@@ -1,5 +0,0 @@
-<?php
-define('root', __DIR__);
-require("core/Kernel.php");
-new Kernel;
-?>
